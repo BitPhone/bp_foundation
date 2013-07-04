5ed1fd4ff0e6a6257fac460654ac84e2dcf52020
Jason J. Gullickson <jason.gullickson@gmail.com>
2013-07-04 14:59:43 -0500

    implemented get messages and get messages test

--- bp_foundation.js
+++ bp_foundation.js
@@ -8,6 +8,47 @@
 	bpGetBalance()
 */
 
+function bpGetMessages(callback){
+
+	// connect to namecoin websocket
+	var bpws = new WebSocket('ws://127.0.0.1:1313');
+
+	// call name_list
+	bpws.onopen = function(){
+
+		bpws.send('name_list');
+
+	};
+
+	bpws.onmessage = function(evt){
+
+		var received_message = JSON.parse(evt.data);
+
+		// convert namecoin objects into a BitPhone messages
+		var bp_messages = [];
+
+		for(i=0;i<received_message.length;i++){
+
+			var bp_message = {};
+
+			bp_message.from = received_message[i].address;
+			bp_message.subject = received_message[i].name;
+			bp_message.body = received_message[i].value;
+
+			bp_messages.push(bp_message);
+		}
+		
+		if(callback){
+			callback(bp_messages);
+		}
+	};
+
+	bpws.onclose = function(){
+		console.log('connection closed');
+	};
+
+}
+
 function bpGetBalance(callback){
 
 	// connect to namecoin websocket
@@ -18,15 +59,15 @@ function bpGetBalance(callback){
 
 		bpws.send('getinfo');
 
-		console.log('message sent');
+		//console.log('message sent');
 	};
 
 	bpws.onmessage = function(evt){
 
 		var received_message = JSON.parse(evt.data);
 
-		console.log('message received: ' + received_message);
-		console.log(received_message);
+		//console.log('message received: ' + received_message);
+		//console.log(received_message);
 
 		if(callback){
 			callback(received_message.balance);
@@ -34,6 +75,6 @@ function bpGetBalance(callback){
 	};
 
 	bpws.onclose = function(){
-		console.log('connection closed');
+		//console.log('connection closed');
 	};
 }
