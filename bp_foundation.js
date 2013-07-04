fa9a3fc0f7b0ba550ab9cbea937bb557e6b8ad64
Jason J. Gullickson <jason.gullickson@gmail.com>
2013-07-04 12:58:37 -0500

    stubbing out functions and added test harness page

--- bp_foundation.js
+++ bp_foundation.js
@@ -1,1 +1,38 @@
 // bp_foundation.js
+
+/*
+	todo:
+	bpSendMessage()
+	bpGetMessages()
+	bpGetNewMessages()
+	bpGetBalance()
+*/
+
+function bpGetBalance(callback){
+
+	// connect to namecoin websocket
+	var bpws = new WebSocket('ws://127.0.0.1:1313');
+
+	// call getinfo
+	bpws.onopen = function(){
+
+		bpws.send('getinfo');
+
+		console.log('message sent');
+	};
+
+	bpws.onmessage = function(evt){
+
+		var received_message = evt.data;
+
+		console.log('message received: ' + received_message);
+
+		if(callback){
+			callback(received_message);
+		}
+	};
+
+	bpws.onclose = function(){
+		console.log('connection closed');
+	};
+}
